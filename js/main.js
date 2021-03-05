340f17e7ac52094a8f057261778743af718a8362
Sebastiano <pasca.sebastiano5@libero.it>
2021-03-05 01:17:16 +0100

    Esercizio + bonus (piccole sistemazioni in corso)

--- js/main.js
+++ js/main.js
@@ -17,40 +17,120 @@
 var mine = [];
 var numeriGiocati = [];
 
-/* do {
-  var difficolta = prompt("Inserisci livello difficoltà: facile, medio o difficile?");
-} while ((difficolta < 0) || (difficolta != "FACILE") || (difficolta != "MEDIO") || (difficolta != "DIFFICILE")); */
+/* 
+-----------------------DO... WHILE NON FUNZIONA--------------------------
+
+do {
+  difficolta = prompt("Inserisci la difficoltà (facile, media o difficile):").toUpperCase();
+} while ((difficolta != "DIFFICILE") || (difficolta != "MEDIA") || (difficolta != "FACILE")); 
+
+------------------------DO... WHILE NON FUNZIONA--------------------------
+*/
+
+
+
+
+
+
+var difficolta = prompt("Inserisci la difficoltà (facile, media o difficile):").toUpperCase();
+/* if ((difficolta != "DIFFICILE") || (difficolta != "MEDIA") || (difficolta != "FACILE")) {
+  alert("Devi inserire un livello di difficoltà"); 
+  
+  //Se lo facessi in questo modo, allora dovrei trovare un modo per fare in modo
+  //di richiedere nuovamente il prompt all'utente se le condizioni sono vere;
+} */
+
+
+// PARTE BONUS
+
+switch(difficolta) {
+
+  case "DIFFICILE":
+    alert("Hai scelto la difficoltà DIFFICILE, e così sei un temerario, eh?");
+    break;
+  case "MEDIA":
+    alert("Hai scelto la difficoltà MEDIA. Mmmh... Sei uno che osa");
+    CPUrandomMedium;
+    break;
+
+  case "FACILE":
+    alert('Hai scelto la difficoltà FACILE, una partita "tranquilla"');
+    CPUrandomEasy;
+}
+
+// FINE PARTE BONUS
 
 
 while (mine.length < 16) {
-  var CPUrandom = randomNumber(1, 100);
-  if (isInArray(mine, CPUrandom) == false) {
-    mine.push(CPUrandom);
+  if (difficolta == "FACILE") {
+    var CPUrandomEasy = randomNumber(1, 100);
+    if (isInArray(mine, CPUrandomEasy) == false) {
+      mine.push(CPUrandomEasy);
+    }
+  }
+  else if (difficolta == "MEDIA") {
+    var CPUrandomMedium = randomNumber(1, 80);
+    if (isInArray(mine, CPUrandomMedium) == false) {
+      mine.push(CPUrandomMedium);
+    }
+  }
+  else if (difficolta == "DIFFICILE") {
+    var CPUrandomHard = randomNumber(1, 50);
+    if (isInArray(mine, CPUrandomHard) == false) {
+      mine.push(CPUrandomHard);
+    }
   }
 }
 console.log("MINE: " + mine);
 
 for (var i = 0; i < 84; i++) {
 
-  var numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 100"));
-  console.log(numeroUtente);
+  if (difficolta == "FACILE") {
+    var numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 100"));
+  }
+  else if (difficolta == "MEDIA") {
+    numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 80"));
+  }
+  else if (difficolta == "DIFFICILE") {
+    numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 50"));
+  }
 
-  if (isNaN(numeroUtente)) { // Queste condizioni le potrei mettere fuori dal ciclo?
+  if (isNaN(numeroUtente)) {
     alert("Attenzione: inserire un numero.");
-  }
-  else if (isInArray(mine, numeroUtente)) { // Perché se posiziono questo else if (riga 37) al posto di quello
-    alert("Hai perso! Ricarica la pagina e ritenta." + " -- Punteggio: " + numeriGiocati.length * 10);// di riga 40 allora non funziona?
-    break;
-  }
-  else if (i == 84) {
-    alert("Complimenti! Hai vinto! -- " + "Punteggio: " + numeriGiocati.length * 10);
+    continue;
   }
   else if (isInArray(numeriGiocati, numeroUtente)) {
     alert("Attenzione: numero già presente");
-    i--;
-  }
+    numeriGiocati.pop(); // In questo modo: scrivo numero e va bene,                   
+  }     //poi lo riscrivo e me lo rimuove, quindi la volta dopo,
+  //riscrivendolo, sarà come non averlo mai scritto, perciò me lo ripusherà
   else {
     numeriGiocati.push(numeroUtente);
+  }
+
+  if ((difficolta == "FACILE") && (numeroUtente >= 101) || (numeroUtente <= 0)) {
+    alert("Numero non consentito");
+    numeriGiocati.pop();
+  }
+
+  if ((difficolta == "MEDIA") && (numeroUtente >= 81) || (numeroUtente <= 0)) {
+    alert("Numero non consentito");
+    numeriGiocati.pop();
+  }
+
+  if ((difficolta == "DIFFICILE") && (numeroUtente >= 51) || (numeroUtente <= 0)) {
+    alert("Numero non consentito");
+    numeriGiocati.pop();
+  }
+
+  if (isInArray(mine, numeroUtente)) {
+    alert("Hai perso! Ricarica la pagina e ritenta." + " -- Punteggio: " + numeriGiocati.length * 10);
+    numeriGiocati.pop();
+    break;
+  }
+
+  if (i == 83) {
+    alert("Complimenti! Hai vinto! -- " + "Punteggio: " + numeriGiocati.length * 10);
   }
 }
 
@@ -58,14 +138,8 @@ console.log("Il tuo punteggio: " + numeriGiocati.length * 10);
 console.log("I TUOI NUMERI ESATTI: " + numeriGiocati);
 
 
-// PARTE BONUS
 
-
-
-
-// FINE PARTE BONUS
-
-
+//FUNZIONI
 
 function randomNumber(min, max) {
   var result = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -74,10 +148,12 @@ function randomNumber(min, max) {
 
 
 function isInArray(array, value) {
-  for (i = 0; i < array.length; i++) {
+  for (var i = 0; i < array.length; i++) {
     if (value == array[i]) {
       return true;
     }
   }
   return false;
 }
+
+// FINE FUNZIONI
